cb8d86ca0f8cc19e5c84740b89e8ec5418eb2d8f
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-03-01 23:24:23 -0500

    function afterNMonths is added

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ function additiveInverse(arr){
 }
 
 console.log(additiveInverse([-5, 7, -8, -3]))
+
+
+// Create a function that takes in year and months as input, then return what yeasr it would be after n-months has elapsed
+// Assume that aading 12 months will always increment the years by 1.
+// If no value is given for yeaar or months, return "years missing" or " month missing".
+// At least one value will be present. 
+
+function afterNMonths(year, months){
+   if (year === undefined){
+       return "Year missing"
+   } else if( months === undefined) {
+       return "Month missing"
+   }else if(Number.isInteger(year) && Number.isInteger(months)){
+       // Number.isInteger() checks if the object is a number or not.
+       let addedMonths = months / 12
+       addedMonths = Math.trunc(addedMonths)
+       // The Math.trunc() function returns the integer part of a number by removing any fractional digits.
+       return year + addedMonths
+   }
+}
+
+console.log(afterNMonths(2009, 24))
