b159f2e1257b6f8fe0c34856109e4b88d5ec23b1
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-25 19:04:25 -0400

    adds timeConversion func.

--- index.js
+++ index.js
@@ -390,3 +390,28 @@ function birthdayCakeCandles(ar) {
 }
 
 birthdayCakeCandles([3,2,3,4])
+
+// FUNCTION - 20
+// Given a time in 12 -hour AM/PM format, convert it to military (24-hour) time.
+//Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
+
+function timeConversion(s) {
+    let time = s.split(":")[0]
+    time = Number(time)
+    let newTime = null
+    let amOrPm = s.split(":")[2].split("").splice(-2, 2)
+    amOrPm = amOrPm.join("")
+    if(amOrPm === "PM" && time < 12 ){
+        newTime = time + 12
+        return s.replace(s.split(":")[0], String(newTime)).replace("PM", "")
+    }
+    else if(amOrPm === "PM" && time === 12){
+        return s.replace("PM", "")
+    }
+    else if(amOrPm === "AM" && time === 12){
+        newTime = "00"
+        return s.replace(s.split(":")[0], newTime).replace("AM", "")
+    } else if(  amOrPm === "AM"  ){
+       return s.replace("AM", "")
+    }
+}
