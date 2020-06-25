75fdf66dec3af7c61cb4fdcb4146d8645d759436
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-25 17:17:41 -0400

    changes on function numbers

--- index.js
+++ index.js
@@ -345,7 +345,7 @@ console.log(filterList([1, 2, 3, 'x', 'y', 10]))
 
 // returns => [1, 2, 3, 10]
 
-// FUNCTION - 17
+// FUNCTION - 18
 // Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 function miniMaxSum(arr) {
     let minNum = Math.min(...arr)
