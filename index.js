12597f7f89b2bb8f3669e9cab1142dd6fd50a5a9
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-06-27 19:40:59 -0400

    adds breakingScores func.

--- index.js
+++ index.js
@@ -479,3 +479,29 @@ function gradingStudents(grades) {
   }
   return newGrades
 }
+
+// FUNCTION - 24
+// Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there.
+
+function breakingRecords(scores) {
+    let worstScore = scores[0]
+    let bestScore = scores[0]
+
+    let timesOfWorst = 0
+    let timesOfBest = 0
+    console.log(scores)
+    for(let i = 1; i < scores.length; i++){
+      if(scores[i] < worstScore){
+         console.log(`${scores[i]} is less then ${worstScore}`,)
+          worstScore = scores[i]
+          timesOfWorst += 1
+          console.log("worst =>", timesOfWorst)
+      } else if(scores[i] > bestScore){
+          console.log(`${scores[i]} is bigger then ${bestScore}`)
+          bestScore = scores[i]
+           timesOfBest += 1
+          console.log("best =>", timesOfBest)
+      }
+    }
+   return `${timesOfBest} ${timesOfWorst}`
+}
